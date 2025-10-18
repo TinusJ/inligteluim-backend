5e3b4aa809d62cf653fb4cc93a8978e02fa0b2c7
Tinus <jacksontinus96@gmail.com>
2025-10-18 12:18:26 +0200

    first commit

---
+++ src/main/java/com/tinusj/inligteluim/InLigteLuimApplication.java
@@ -0,0 +1,13 @@
+package com.tinusj.inligteluim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InLigteLuimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InLigteLuimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tinusj/inligteluim/InLigteLuimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinusj.inligteluim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InLigteLuimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
